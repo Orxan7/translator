3d3e9022f981d8db15100cb56f348da28c54141d
Orkhan Shahverdiyev <31187214+Orxan7@users.noreply.github.com>
2025-01-29 22:02:48 +0400

    Added command for set emoji

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import random
 import telebot
+from telebot.types import ReactionTypeEmoji
 
-token = "11111111"
+API_TOKEN = '7808074378:AAHxYHMinPfn7LbV_U4u7gqyPg7Nu3WRKHA'
+bot = telebot.TeleBot(API_TOKEN)
 
 
-Добавил новую функцию
+# Send a reactions to all messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
+
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
